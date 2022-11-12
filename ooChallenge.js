7f2d193927f2270375563e3363b4484efdc3c613
Youssef Doss <youssefdoss97@gmail.com>
2022-11-11 22:37:29 -0500

    build various vehicle classes

--- ooChallenge.js
+++ ooChallenge.js
@@ -14,3 +14,39 @@ class Vehicle {
         return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
     }
 }
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine() {
+        return 'VROOM!!!';
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(vehicle) {
+        if (vehicle instanceof Vehicle && this.vehicles.length < this.capacity) {
+            this.vehicles.push(vehicle);
+            return 'Vehicle Added!';
+        } else if (!(vehicle instanceof Vehicle)) {
+            throw new Error('Only vehicles are allowed in here!');
+        } else {
+            throw new Error(`Sorry, we're full.`);
+        }
+    }
+}

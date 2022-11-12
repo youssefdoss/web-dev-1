08206666a527bec8d5a57032d6f691fafb537326
Youssef Doss <youssefdoss97@gmail.com>
2022-11-12 00:42:52 -0500

    finished almostSorted and started findMultiples

--- findMultiples.js
+++ findMultiples.js
@@ -1,2 +1,43 @@
-function findMultiples() {
+/**
+ * findNumFreqs: Creates a frequency counter for the number of times
+ * each number appears in the given array
+ * 
+ * nums: array
+ * 
+ * return: object
+ */
+function findNumFreqs(nums) {
+    let freqs = {};
+
+    for (let num of nums) {
+        const curr = freqs[num] || 0;
+        freqs[num] = curr + 1;
+    }
+
+    return freqs;
+}
+
+/**
+ * findMultiples: returns an array of the numbers found in an array of numbers
+ * that appear more than the designated minimum number of times
+ * 
+ * nums: array
+ * minimumCount; number
+ * 
+ * return: array
+ */
+
+// TODO: MAKE IT RUN FASTER IT CAN'T USE INCLUDES
+function findMultiples(nums, minimumCount) {
+    const numFreqs = findNumFreqs(nums);
+    let numerousNums = [];
+
+    for (let num of nums) {
+
+        if (numFreqs[num] >= minimumCount && !numerousNums.includes(num)) {
+            numerousNums.push(num);
+        }
+    }
+
+    return numerousNums;
 }
--- sortAlmostSorted.js
+++ sortAlmostSorted.js
@@ -1,2 +1,26 @@
-function sortAlmostSorted() {
+/**
+ * sortAlmostSorted: takes an array of almost sorted numbers with
+ * one pair of numbers out of order and returns a sorted array
+ * 
+ * almostSortedNums: array
+ * 
+ * return: array
+ */
+function sortAlmostSorted(almostSorted) {
+    for (let i = almostSorted.length - 1; i > 0; i--) {
+        
+        if (almostSorted[i] < almostSorted[i - 1]) {
+            let j = i - 1;
+
+            while (j > -1 && almostSorted[i] < almostSorted[j]) {
+                j--;
+            }
+
+            [almostSorted[i], almostSorted[j + 1]] = [almostSorted[j + 1], almostSorted[i]];
+
+            break;
+        }
+    }
+    
+    return almostSorted;
 }
